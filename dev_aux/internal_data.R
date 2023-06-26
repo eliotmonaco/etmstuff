8c366a47d8a1addef291b2118a1c5c5b17224f48
Eliot Monaco <eliot.monaco@ks.gov>
2023-06-26 10:53:05 -0500

    Added fips to internal data

--- dev_aux/internal_data.R
+++ dev_aux/internal_data.R
@@ -1,37 +1,53 @@
 # Generate internal data for package
 
+common_streets <- readRDS("dev_aux/helpers/common_streets.rds")
+common_street_suffixes <- readRDS("dev_aux/helpers/common_street_suffixes.rds")
+common_unit_prefixes <- readRDS("dev_aux/helpers/common_unit_prefixes.rds")
+directions <- readRDS("dev_aux/helpers/directions.rds")
 directions_cardinal <- readRDS("dev_aux/helpers/directions_cardinal.rds")
 directions_ordinal <- readRDS("dev_aux/helpers/directions_ordinal.rds")
 epitrax_variables <- readRDS("dev_aux/helpers/epitrax_variables.rds")
 epitrax_variables_reordered <- readRDS("dev_aux/helpers/epitrax_variables_reordered.rds")
-pm_direction_dictionary <- readRDS("dev_aux/helpers/pm_direction_dictionary.rds")
+fips <- readRDS("dev_aux/helpers/fips.rds")
 ks_cities <- readRDS("dev_aux/helpers/ks_cities.rds")
 ks_zipcodes <- readRDS("dev_aux/helpers/ks_zipcodes.rds")
+pm_direction_dictionary <- readRDS("dev_aux/helpers/pm_direction_dictionary.rds")
 regex_pobox <- readRDS("dev_aux/helpers/regex_pobox.rds")
 regex_various <- readRDS("dev_aux/helpers/regex_various.rds")
 street_suffix <- readRDS("dev_aux/helpers/street_suffix.rds")
-directions <- readRDS("dev_aux/helpers/directions.rds")
-common_streets <- readRDS("dev_aux/helpers/common_streets.rds")
-common_street_suffixes <- readRDS("dev_aux/helpers/common_street_suffixes.rds")
-common_unit_prefixes <- readRDS("dev_aux/helpers/common_unit_prefixes.rds")
 
 usethis::use_data(
+  common_streets,
+  common_street_suffixes,
+  common_unit_prefixes,
+  directions,
   directions_cardinal,
   directions_ordinal,
   epitrax_variables,
   epitrax_variables_reordered,
-  pm_direction_dictionary,
+  fips,
   ks_cities,
   ks_zipcodes,
+  pm_direction_dictionary,
   regex_pobox,
   regex_various,
   street_suffix,
-  directions,
-  common_streets,
-  common_street_suffixes,
-  common_unit_prefixes,
   internal = T, overwrite = T
 )
+
+
+
+# Modify fips ####
+
+fips <- read.csv("dev_aux/helpers/fips.csv")
+colnames(fips) <- tolower(colnames(fips))
+colnames(fips)
+colnames(fips) <- c("fips", "st_fips", "cnty_fips", "cnty_code", "state", "county")
+fips$cnty_fips <- as.character(substr(fips$fips, 3, 5))
+fips$fips <- as.character(fips$fips)
+fips$st_fips <- as.character(fips$st_fips)
+
+saveRDS(fips, "dev_aux/helpers/fips.rds")
 
 
 
