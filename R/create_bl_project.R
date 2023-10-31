b4d53bcd5bf6c37c3d3daebffa7a4442dce44698
Eliot Monaco <eliot.monaco@ks.gov>
2023-10-31 09:22:28 -0500

    Updated subdirectories created by create_bl_project(), and changed function name

--- R/create_bl_project.R
+++ R/create_bl_project.R
@@ -1,26 +1,27 @@
-#' Create blood lead project and subdirectories
+#' Create blood lead project and directories
 #'
-#' This function creates an R project and subdirectories for a blood lead data processing project. If `parent_dir` is not found or if a project directory with the same name already exists, the function will error out before creating anything. The R project is created using [usethis::create_project()].
+#' This function creates an R project and all directories for a blood lead data processing project. If `parent_dir` is not found or if a project directory with the same name already exists, the function will error out before creating anything. The R project is created using [usethis::create_project()].
 #'
-#' @param parent_dir The path to the directory in which the BL project will reside.
-#' @param data_range The date range of the BL data that will be processed.
+#' @param data_range The date range of the lead data that will be processed.
 #'
 #' * For one quarter, use the format YYYYqQ (e.g., "2023q1")
 #' * For one year, use the format YYYY (e.g., "2023")
 #' * For a range that spans multiple years, use the format YYYY-YYYY (e.g., "2015-2022")
+#'
+#' @param parent_dir The path to the directory in which the project will reside.
 #'
 #' @return A message indicating success or failure in creating all directories.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' create_bl_data_project(
+#' create_bl_project(
 #'   parent_dir = "~/my_projects/",
 #'   data_range = "2023q1"
 #' )
 #' }
 #'
-create_bl_data_project <- function(parent_dir, data_range) {
+create_bl_project <- function(data_range, parent_dir = "C:/Users/eliot.monaco/OneDrive - State of Kansas, OITS/Documents/r_projects") {
   if (!dir.exists(parent_dir)) {
     stop("`parent_dir` not found", call. = FALSE)
   }
@@ -53,6 +54,7 @@ create_bl_data_project <- function(parent_dir, data_range) {
     "data/intermediate",
     "data/registries",
     "output",
+    "output/addresses",
     "output/cbls",
     "output/epitrax",
     "scripts"
