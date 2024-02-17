1413f041fa043ec874538a43aaa0ab56daf85717
eliotmonaco <eliotmonaco@gmail.com>
2024-02-17 15:38:40 -0600

    Changed argument names in calculate_age.R

--- R/calculate_age.R
+++ R/calculate_age.R
@@ -1,9 +1,9 @@
 #' Calculate age
 #'
-#' Calculate age from two date vectors. The formula used is `(d2 - d1) / 365.25`.
+#' Calculate age from two date vectors. The formula used is `(date2 - date1) / 365.25`.
 #'
-#' @param d1 A vector containing the birth or start date for the age calculation.
-#' @param d2 A vector containing the end date for the age calculation.
+#' @param date1 A vector containing the birth or start date for the age calculation.
+#' @param date2 A vector containing the end date for the age calculation.
 #'
 #' @return A numeric vector.
 #' @export
@@ -27,23 +27,23 @@
 #'   size = 10
 #' )
 #'
-#' calculate_age(d1 = start, d2 = end)
+#' calculate_age(date1 = start, date2 = end)
 #'
-calculate_age <- function(d1, d2) {
+calculate_age <- function(date1, date2) {
   # Check if vectors are formatted as dates
-  if (!lubridate::is.Date(d1) | !lubridate::is.Date(d2)) {
-    stop("`d1` and `d2` must be formatted as dates")
+  if (!lubridate::is.Date(date1) | !lubridate::is.Date(date2)) {
+    stop("`date1` and `date2` must be formatted as dates")
   }
 
   # Check if vectors are the same length
-  if (length(d1) != length(d2)) {
-    stop("`d1` and `d2` must be the same length")
+  if (length(date1) != length(date2)) {
+    stop("`date1` and `date2` must be the same length")
   }
 
   # Check for NAs
-  if (any(is.na(d1))) message("NAs found in `d1`")
-  if (any(is.na(d2))) message("NAs found in `d2`")
+  if (any(is.na(date1))) message("NAs found in `date1`")
+  if (any(is.na(date2))) message("NAs found in `date2`")
 
   # Calculate age
-  as.numeric((d2 - d1) / 365.25)
+  as.numeric((date2 - date1) / 365.25)
 }
